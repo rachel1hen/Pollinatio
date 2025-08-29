bb2517fc59d51afc6695b85f00b721656e4f296d
rachel1hen <race2towin@gmail.com>
2025-08-29 23:50:37 +0530

    Update Stich.py

--- scripts/Stich.py
+++ scripts/Stich.py
@@ -9,3 +9,14 @@ import requests
 
 TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
 TELEGRAM_CHAT_ID = "-1002386494312"
+
+def main():
+    chapter_arg = os.getenv("CHAPTER_NUM")
+    #chapter, idx, lines = pick_chapter(chapter_arg)
+    #if not chapter:
+    #    print("No chapters pending")
+    #    return
+    #asyncio.run(process_chapter(chapter, idx, lines))
+
+if __name__ == "__main__":
+    main()
